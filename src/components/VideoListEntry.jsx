5d13773d3aaab1bfd7d93220513dd2c8a5b91bcc
Zynh Ludwig <Zynh0722@gmail.com>
2022-03-07 17:12:19 -0800

    Finish all none youtube api stuff

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,11 +1,11 @@
-var VideoListEntry = () => (
+var VideoListEntry = (p) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+      <img className="media-object" src={p.video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
+      <div className="video-list-entry-title" onClick={p.entryClick.bind(null, p.index)} >{p.video.snippet.title}</div>
+      <div className="video-list-entry-detail">{p.video.snippet.description}</div>
     </div>
   </div>
 );
@@ -14,6 +14,10 @@ var VideoListEntry = () => (
 // Warnings will be shown in the console when the defined rules are violated
 VideoListEntry.propTypes = {
   video: PropTypes.object.isRequired
+};
+
+VideoListEntry.defaultProps = {
+  entryClick: ()=>{}
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
